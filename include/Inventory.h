93d18682bf028ad4cf97fcb80af28093a0b721f2
youwenhei <youwenhei1110@gmail.com>
2026-07-08 22:56:51 +0800

    Add newProductID and constructor to Inventory class

--- include/Inventory.h
+++ include/Inventory.h
@@ -9,9 +9,13 @@ class Inventory
 {
 private:
 	std::vector<Product> products; // Vector to store the list for products dynamically
+	int newProductID; // Initialize the product ID counter
 
 public:
+	Inventory();
+	void addProduct(); //function overloading
 	void addProduct(const Product& product);
+
 	void displayProducts() const;
 };
 

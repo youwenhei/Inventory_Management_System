b5a4ab878f8398382208a31937644b50754ac6d0
youwenhei <youwenhei1110@gmail.com>
2026-07-08 21:33:37 +0800

    Implement Inventory class with product management
    Added Inventory class with methods to add and display products.

--- include/Inventory.h
+++ include/Inventory.h
@@ -1,4 +1,18 @@
 #ifndef INVENTORY_H
 #define INVENTORY_H
 
+#pragma once
+#include <vector>
+#include "Product.h"
+
+class Inventory
+{
+private:
+	std::vector<Product> products; // Vector to store the list for products dynamically
+
+public:
+	void addProduct(const Product& product);
+	void displayProducts() const;
+};
+
 #endif
